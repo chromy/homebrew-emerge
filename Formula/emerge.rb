f701ee5fcff15db8541c1aa3db8c7169465f3eb8
Hector <hector@emergetools.com>
2024-08-12 09:46:12 +0100

    update to v0.0.3

--- Formula/emerge.rb
+++ Formula/emerge.rb
@@ -1,7 +1,7 @@
 class Emerge < Formula
   desc "Emergetools CLI"
   homepage "https://docs.emergetools.com"
-  url "https://github.com/chromy/emerge-cli/archive/refs/tags/v0.0.2.tar.gz"
+  url "https://github.com/chromy/emerge-cli/archive/refs/tags/v0.0.3.tar.gz"
   sha256 "39cdd1d08eb87a0edb27fd3ffaabb5a8a7e1f71b491f7eb61d577b972810bcd1"
   license "MIT"
 
