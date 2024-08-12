ddef2c24b961192989d1e272d40f28d84ba8dba2
Hector <hector@emergetools.com>
2024-08-12 09:00:47 +0100

    Only build MacOS

--- Formula/emerge.rb
+++ Formula/emerge.rb
@@ -1,6 +1,6 @@
 class Emerge < Formula
-  desc ""
-  homepage ""
+  desc "Emergetools CLI"
+  homepage "https://docs.emergetools.com"
   url "https://github.com/chromy/emerge-cli/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "39cdd1d08eb87a0edb27fd3ffaabb5a8a7e1f71b491f7eb61d577b972810bcd1"
   license "MIT"
@@ -19,15 +19,6 @@ class Emerge < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test emerge`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
